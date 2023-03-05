dee00576418d0896750c0f4de55f4e3c3d5c365a
jwseph <kodomo.oishii@gmail.com>
2023-03-05 11:19:56 -0800

    Improvement

--- harvest_season.cpp
+++ harvest_season.cpp
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int D[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int K;
+bool V[5][5]{};
 
-int K, res = 0;
-bool E[5][5]{};
-
-void dfs(int r, int c, int l) {
-    if (!(0 <= r && r < 5 && 0 <= c && c < 5) || E[r][c]) return;
+int dfs(int r, int c) {
+    if (!(0 <= r && r < 5 && 0 <= c && c < 5) || V[r][c]) {
+        return 0;
+    }
     if (r == 4 && c == 4) {
-        if (l == 5*5-K) ++res;
-        return;
+        return K == 25-1;
     }
-    E[r][c] = true;
-    for (auto& d: D) {
-        dfs(r+d[0], c+d[1], l+1);
-    }
-    E[r][c] = false;
+    V[r][c] = true; ++K;
+    int res = dfs(r-1, c)+dfs(r+1, c)+dfs(r, c-1)+dfs(r, c+1);
+    V[r][c] = false; --K;
+    return res;
 }
 
 int main() {
     cin >> K;
     for (int i = 0; i < K; ++i) {
         int r, c; cin >> r >> c; --r; --c;
-        E[r][c] = true;
+        V[r][c] = true;
     }
-    dfs(0, 0, 1);
-    cout << res << endl;
+    cout << dfs(0, 0) << endl;
 }
