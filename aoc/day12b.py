ce7287d4cff048b54d4a1387efec19c1ba3f28ef
Joseph Jackson <kodomo.oishii@gmail.com>
2023-12-11 21:46:10 -0800

    Day 12

--- aoc/day12b.py
+++ aoc/day12b.py
@@ -2,28 +2,6 @@ from utils import *
 
 lines = open('aoc/day12.in').read().splitlines()
 
-def dfs(A, B, i):
-    if i == len(A):
-        cur = 0
-        j = 0
-        for c in A:
-            if c == '.':
-                if cur:
-                    if j == len(B) or B[j] != cur: return 0
-                    j += 1
-                cur = 0
-            else:
-                cur += 1
-        return int(j == len(B))
-    if A[i] != '?': return dfs(A, B, i+1)
-    res = 0
-    A[i] = '#'
-    res += dfs(A, B, i+1)
-    A[i] = '.'
-    res += dfs(A, B, i+1)
-    A[i] = '?'
-    return res
-
 def sol(line):
     S, other = line.split()
     B = list(map(int, other.split(',')))*5
