105f57dd6c581a9d62bd40889ca6e09ee1d1aeba
Joseph Jackson <kodomo.oishii@gmail.com>
2023-12-22 16:00:32 -0800

    ordered set

--- template.cpp
+++ template.cpp
@@ -7,6 +7,13 @@ using namespace std;
 #define pb push_back
 #define all(v) (v).begin(), (v).end()
 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 const int d4r[4] = {0, 1, 0, -1}, d4c[4] = {1, 0, -1, 0}
 const int d8r[8] = {0, -1, -1, -1, 0, 1, 1, 1}, d8c[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 
