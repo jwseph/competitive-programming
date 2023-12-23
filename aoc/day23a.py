3784812815d1f23c56abb6502ccee5d1a830e14c
Joseph Jackson <kodomo.oishii@gmail.com>
2023-12-23 12:15:58 -0800

    Day 23

---
+++ aoc/day23a.py
@@ -0,0 +1,25 @@
+from utils import *
+
+A = open('aoc/day23.in').read().splitlines()
+R = len(A)
+C = len(A[0])
+from math import inf
+V = set()
+
+def dfs(r, c):
+    if r == R-1 and c == C-2: return 0
+    if r < 0 or r >= R or c < 0 or c >= C: return -inf
+    if A[r][c] == '#': return -inf
+    if (r, c) in V: return -inf
+    V.add((r, c))
+    if A[r][c] == 'v': res = 1+dfs(r+1, c)
+    elif A[r][c] == '>': res =  1+dfs(r, c+1)
+    elif A[r][c] == '<': res =  1+dfs(r, c-1)
+    elif A[r][c] == '^': res = 1+dfs(r-1, c)
+    else:
+        res = max(dfs(r+1, c), dfs(r, c+1), dfs(r-1, c), dfs(r, c-1))+1
+    V.remove((r, c))
+    return res
+
+res = dfs(0, 1)
+print(res)
--- template.cpp
+++ template.cpp
@@ -7,11 +7,9 @@ using namespace std;
 #define pb push_back
 #define all(v) (v).begin(), (v).end()
 
-
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
 template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int d4r[4] = {0, 1, 0, -1}, d4c[4] = {1, 0, -1, 0}
